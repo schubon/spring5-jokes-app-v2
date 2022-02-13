58c3f9ae193cc1b4075f18670d16767780cfb5c9
schubon <7129273+schubon@users.noreply.github.com>
2022-02-13 15:04:44 +0100

    Cleaning the controller code

--- src/main/java/de/schubon/spring5jokesapp/controller/JokesController.java
+++ src/main/java/de/schubon/spring5jokesapp/controller/JokesController.java
@@ -1,22 +1,20 @@
 package de.schubon.spring5jokesapp.controller;
 
 import de.schubon.spring5jokesapp.services.JokesService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
 
     private final JokesService jokesService;
 
-    @Autowired(required = true)
     public JokesController(JokesService jokesService) {
         this.jokesService = jokesService;
     }
 
-    @GetMapping(path = "/")
+    @RequestMapping("/")
     public String getJoke(Model model) {
         model.addAttribute("joke", jokesService.produceJoke());
         return "index";
