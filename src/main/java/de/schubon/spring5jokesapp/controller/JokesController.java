72714b7e138618f0b89e5e7e5f4e0ac1fd6631a5
schubon <7129273+schubon@users.noreply.github.com>
2022-02-13 18:25:14 +0100

    Explicit @Autowired and array of request endpoints

--- src/main/java/de/schubon/spring5jokesapp/controller/JokesController.java
+++ src/main/java/de/schubon/spring5jokesapp/controller/JokesController.java
@@ -1,6 +1,7 @@
 package de.schubon.spring5jokesapp.controller;
 
 import de.schubon.spring5jokesapp.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,13 @@ public class JokesController {
 
     private final JokesService jokesService;
 
+    @Autowired
     public JokesController(JokesService jokesService) {
         this.jokesService = jokesService;
     }
 
-    @RequestMapping("/")
-    public String getJoke(Model model) {
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
         model.addAttribute("joke", jokesService.produceJoke());
         return "index";
     }
